aa2257fe4c037135a29f8cafa322b2812dc46e61
md ashraf Uddin <mdtanjil01753511918@gmail.com>
2019-06-04 00:34:17 +0600

    Created Migration

---
+++ database/migrations/2019_06_03_162526_create_replies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned();
+
+            $table->integer('user_id');
+
+            $table->foreign('question_id')->reference('id')->on('questions')
+            ->onDelete('casecade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
